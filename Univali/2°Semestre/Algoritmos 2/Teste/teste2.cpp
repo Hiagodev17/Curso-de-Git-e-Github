7648065babf6233b3e36aeb27f3ad4da87f482d4
Hiagodev17 <hiagogomes.dev@gmail.com>
2026-08-31 18:11:26 -0300

    teste03

--- Univali/2°Semestre/Algoritmos 2/Teste/teste2.cpp
+++ Univali/2°Semestre/Algoritmos 2/Teste/teste2.cpp
@@ -5,4 +5,5 @@ int main(){
     cout << "Hello, World!" << endl;
     cout << "This is a simple C++ program." << endl;
     cout << "It demonstrates basic input and output operations." << endl;
+    cout << "You can modify this program to perform various tasks." << endl;
 }
