eadd09606de72cbd7c3b22f78aefb9b14547d818
Hiagodev17 <hiagogomes.dev@gmail.com>
2026-08-31 18:00:01 -0300

    teste02

--- Univali/2°Semestre/Algoritmos 2/Teste/teste2.cpp
+++ Univali/2°Semestre/Algoritmos 2/Teste/teste2.cpp
@@ -4,4 +4,5 @@ using namespace std;
 int main(){
     cout << "Hello, World!" << endl;
     cout << "This is a simple C++ program." << endl;
+    cout << "It demonstrates basic input and output operations." << endl;
 }
