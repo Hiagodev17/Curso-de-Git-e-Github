1726675e7ced971a2ad4eb995fa5dd704ae465e7
Hiagodev17 <hiagogomes.dev@gmail.com>
2026-08-31 18:05:19 -0300

    teste03

---
+++ Univali/2°Semestre/Algoritmos 2/Teste/teste.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i = 0; i < 100; i++) {
+        cout << "Iteration: " << i << endl;
+        cout << "Hello, World!" << endl;
+    }
+    
+    return 0;
+}
